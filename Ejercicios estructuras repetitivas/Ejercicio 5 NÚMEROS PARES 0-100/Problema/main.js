57f33870c9b5d987ba93e3d9ea1cf5262c59f29c
Sara Diaz Oporto <Sara Diaz Oporto>
2016-06-19 22:03:30 -0500

    commit

--- Ejercicios estructuras repetitivas/Ejercicio 5 NÚMEROS PARES 0-100/Problema/main.js
+++ Ejercicios estructuras repetitivas/Ejercicio 5 NÚMEROS PARES 0-100/Problema/main.js
@@ -1,16 +1,11 @@
-function par(){
+function miFuncion(){
 
 	var lista=[];
+	var ul = document.getElementById("miLista");
 for (var i =1; i <= 100; i++) {
 	var ul=document.getElementById("lista");
 	if(i%2===0){
-		for (var a=0; a<=100; a++) {
-			lista[a]=i;
-			muestra=lista[a];
-	var li=document.createElement("li");
-li.appendChild(document.createTextNode(muestra));
-ul.appendChild(li);
-		}
-	}
+alert(i);
+}
 }
 }
